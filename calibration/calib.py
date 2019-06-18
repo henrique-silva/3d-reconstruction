16b81ca68abd7bb2823415e67bd3435fb3a2fed0
Henrique Silva <henrique812@gmail.com>
2019-06-18 14:14:30 -0300

    Use chessboard size as tuple variable

--- calibration/calib.py
+++ calibration/calib.py
@@ -2,12 +2,13 @@ import numpy as np
 import cv2
 import glob
 
+chessboard_size = (7,9)
 # termination criteria
 criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
 
 # prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
-objp = np.zeros((6*9,3), np.float32)
-objp[:,:2] = np.mgrid[0:9,0:6].T.reshape(-1,2)
+objp = np.zeros((np.prod(chessboard_size),3),dtype=np.float32)
+objp[:,:2] = np.mgrid[0:chessboard_size[0], 0:chessboard_size[1]].T.reshape(-1,2)
 
 debug_show = True 
 # Arrays to store object points and image points from all the images.
@@ -23,7 +24,7 @@ for fname in images:
     grayL = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
 
     # Find the chess board corners
-    ret, cornersL = cv2.findChessboardCorners(grayL, (6,9),None)
+    ret,corners = cv2.findChessboardCorners(gray, chessboard_size, None)
     # If found, add object points, image points (after refining them)
     if ret == True:
         objpointsL.append(objp)
@@ -32,7 +33,7 @@ for fname in images:
         imgpointsL.append(cornersL)
         if debug_show:
             # Draw and display the corners
-            img = cv2.drawChessboardCorners(img, (6,9), cornersL,ret)
+            img = cv2.drawChessboardCorners(img, chessboard_size, corners,ret)
             cv2.imshow('img',img)
             cv2.waitKey(1000)
 
