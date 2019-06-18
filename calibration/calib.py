1f184d95738c275bd284f7d9bb487b16bbb0076a
Henrique Silva <henrique812@gmail.com>
2019-06-18 14:16:56 -0300

    Calibrate only once

--- calibration/calib.py
+++ calibration/calib.py
@@ -26,69 +26,52 @@ objp[:,:2] = np.mgrid[0:chessboard_size[0], 0:chessboard_size[1]].T.reshape(-1,2
 
 debug_show = True 
 # Arrays to store object points and image points from all the images.
-objpointsL = [] # 3d point in real world space
-imgpointsL = [] # 2d points in image plane.
-objpointsR = []
-imgpointsR = []
+objpoints = [] # 3d point in real world space
+imgpoints = [] # 2d points in image plane.
 
 images = sorted(glob.glob('chess/left/*.jpg'))
 
 for fname in images:
+    print('Calibrating with image '+fname)
     img = cv2.imread(fname)
-    grayL = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+    img = downsample_image(img, 2)
 
+    gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+    cv2.imshow('gray',gray)
+    cv2.waitKey(10)
     # Find the chess board corners
     ret,corners = cv2.findChessboardCorners(gray, chessboard_size, None)
     # If found, add object points, image points (after refining them)
     if ret == True:
-        objpointsL.append(objp)
+        objpoints.append(objp)
 
-        cornersL = cv2.cornerSubPix(grayL,cornersL,(11,11),(-1,-1),criteria)
-        imgpointsL.append(cornersL)
+        corners = cv2.cornerSubPix(gray,corners,(11,11),(-1,-1),criteria)
+        imgpoints.append(corners)
         if debug_show:
             # Draw and display the corners
             img = cv2.drawChessboardCorners(img, chessboard_size, corners,ret)
             cv2.imshow('img',img)
-            cv2.waitKey(1000)
+            cv2.waitKey(10)
+    else:
+        print('Could not find points in image!')
+ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(objpoints, imgpoints, gray.shape[::-1], None, None)
 
-retL, mtxL, distL, rvecsL, tvecsL = cv2.calibrateCamera(objpointsL, imgpointsL, grayL.shape[::-1], None, None)
-#print(retL, mtxL, distL, rvecsL, tvecsL)
 #Get exif data in order to get focal length.
 exif_img = PIL.Image.open(images[0])
 
-#Right view calibration
-images = sorted(glob.glob('chess/right/*.jpg'))
 exif_data = {
 	PIL.ExifTags.TAGS[k]:v
 	for k, v in exif_img._getexif().items()
 	if k in PIL.ExifTags.TAGS}
 
-for fname in images:
-    img = cv2.imread(fname)
-    grayR = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
 #Get focal length in tuple form
 focal_length_exif = exif_data['FocalLength']
 
-    # Find the chess board corners
-    ret, cornersR = cv2.findChessboardCorners(grayR, (6,9),None)
 #Get focal length in decimal form
 focal_length = float(focal_length_exif[0])/focal_length_exif[1]
 
-    # If found, add object points, image points (after refining them)
-    if ret == True:
-        objpointsR.append(objp)
 
-        cornersR = cv2.cornerSubPix(grayR,cornersR,(11,11),(-1,-1),criteria)
-        imgpointsR.append(cornersR)
 
-        if debug_show:
-            # Draw and display the corners
-            img = cv2.drawChessboardCorners(img, (6,9), cornersR,ret)
-            cv2.imshow('img',img)
-            cv2.waitKey(1000)
-
-retR, mtxR, distR, rvecsR, tvecsR = cv2.calibrateCamera(objpointsR, imgpointsR, grayR.shape[::-1], None, None)
-#print(retR, mtxR, distR, rvecsR, tvecsR)
 
 
 #Undistort test
@@ -108,6 +91,4 @@ for fname in images:
     cv2.waitKey(1000)
     raw_input('')
 
-
-#retval,cameraMatrix1, distCoeffs1, cameraMatrix2, distCoeffs2, R, T, E, F = cv2.stereoCalibrate(objpointsL, imgpointsL, imgpointsR, (320,240))
 cv2.destroyAllWindows()
